c735a6c606b1b6634b484e86378de0b78fc58343
Катерина <77587548+KaterinaSavina@users.noreply.github.com>
2024-02-03 20:37:14 +0300

    Create Fibonacci.py

---
+++ Fibonacci.py
@@ -0,0 +1,17 @@
+ef fib(n):
+    if (n < 0):
+        return("Index Error")
+    else:
+        index = 0
+        fib = 0
+        fib_new = 0
+        middle = 1
+        while index < n:
+            fib_new = fib + middle
+            fib = middle
+            middle = fib_new
+            index = index + 1
+            print(fib)
+        fib_new = str(fib)
+        return(fib_new)
+print(fib(8))
